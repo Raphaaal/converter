761f5211e24a988e7bc16c4fec812669685396b2
azorra17 <azorra17@b028-10.crio.dauphine.fr>
2019-03-22 11:25:35 +0100

    added toPdf working class

--- src/main/java/toPdf/toPdf.java
+++ src/main/java/toPdf/toPdf.java
@@ -1,14 +1,14 @@
 package toPdf;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 
+import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xwpf.converter.pdf.PdfConverter;
 import org.apache.poi.xwpf.converter.pdf.PdfOptions;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-
-import fr.opensagres.xdocreport.docx.converters.Data;
 
 public class toPdf
 {
@@ -20,10 +20,11 @@ public class toPdf
         try
         {
             // 1) Load docx with POI XWPFDocument
-            XWPFDocument document = new XWPFDocument( Data.class.getResourceAsStream( "DocxBig.docx" ) );
+       	 FileInputStream fis = new FileInputStream("test.docx");
+    	 XWPFDocument document=new XWPFDocument(OPCPackage.open(fis));
 
             // 2) Convert POI XWPFDocument 2 PDF with iText
-            File outFile = new File( "target/DocxBig.pdf" );
+            File outFile = new File( "target/test.pdf" );
             outFile.getParentFile().mkdirs();
 
             OutputStream out = new FileOutputStream( outFile );
