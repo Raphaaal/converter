c45786fdd8430af8fc12e0ac25507378a9e6f4b1
azorra17 <azorra17@b028-26.crio.dauphine.fr>
2019-04-12 09:17:58 +0200

    Firt use of ConvertAPI to PDF

--- src/main/java/toPdf/toPdf.java
+++ src/main/java/toPdf/toPdf.java
@@ -10,30 +10,14 @@ import org.apache.poi.xwpf.converter.pdf.PdfConverter;
 import org.apache.poi.xwpf.converter.pdf.PdfOptions;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
+import com.convertapi.ConvertApi;
+
 public class toPdf
 {
 
-    public static void main( String[] args )
-    {
-        long startTime = System.currentTimeMillis();
-
-        try
-        {
-            // 1) Load docx with POI XWPFDocument
-       	 FileInputStream fis = new FileInputStream("test.docx");
-    	 XWPFDocument document=new XWPFDocument(OPCPackage.open(fis));
-
-            // 2) Convert POI XWPFDocument 2 PDF with iText
-            File outFile = new File( "target/test.pdf" );
-            outFile.getParentFile().mkdirs();
-
-            OutputStream out = new FileOutputStream( outFile );
-            PdfOptions options = PdfOptions.create().fontEncoding( "windows-1250" );
-            PdfConverter.getInstance().convert( document, out, options );
-        }
-        catch ( Throwable e )
-        {
-            e.printStackTrace();
-        }
+    public static void main( String[] args ) {
+        ConvertApi.convertFile("test.rtf", "result_rtf.pdf", "3q5DgWSGJshJhRKA");
+        System.out.println("Convesion done.");
     }
+    
 }
